a6d0cb947f0f33cc036f3b275916b10c8be27045
Talisca <luiz.f.araujo9@ba.estudante.senai.br>
2026-09-25 19:13:15 -0300

    Criado a Model com as propriedades dos atributos da tabela do banco de dados

---
+++ MinhaApi/Models/Departamento.cs
@@ -0,0 +1,12 @@
+namespace MinhaApi.Models;
+
+public class Departamento
+{
+    public int Id {get; set;}
+    public string Nome {get; set;}
+        = string.Empty;
+    public string Descricao {get; set;}
+        = string.Empty;
+    public bool Ativo {get; set;}
+        = true;
+}

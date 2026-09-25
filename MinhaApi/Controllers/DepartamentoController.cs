ceb647047ed3a27f55c9e6f28619ddefbc37fa8c
Talisca <luiz.f.araujo9@ba.estudante.senai.br>
2026-09-25 20:07:05 -0300

    Feito a Service e começarei a implementar a Controller

---
+++ MinhaApi/Controllers/DepartamentoController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using MinhaApi.Models;
+using MinhaApi.Services;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class DepartamentoController : ControllerBase
+{
+    private readonly IDepartamentoService _service;
+
+    public DepartamentoController(IDepartamentoService service)
+    => _service = service;
+
+    //GET / api/produto
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var fornecedor = _service.GetAll();
+        return Ok(fornecedor);
+    }
+
+// GET /api/produto/1
+    [HttpGet("id")]
+    public IActionResult GetById(int id)
+    {
+        var fornecedor = _service.GetById(id);
+        if (fornecedor == null)
+            return NotFound();
+        return Ok(fornecedor);
+    }
+
+// POST /api/produto
+    [HttpPost]
+    public IActionResult Create([FromBody] Fornecedor fornecedor)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var criado = _service.Create(fornecedor);
+
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = fornecedor.Id },
+            criado);
+    }
+// PUT /api/produto/1
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, [FromBody] Fornecedor fornecedor)
+    {
+        var atualizado = _service.Update(id, fornecedor);
+
+        if (atualizado == null)
+            return NotFound();
+
+        return Ok(atualizado);
+    }
+
+// DELETE /api/produto/1
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var deletado = _service.Delete(id);
+
+        if (!deletado)
+            return NotFound();
+
+        return NoContent();
+    }    
+}
---
+++ MinhaApi/Services/DepartamentoService.cs
@@ -0,0 +1,47 @@
+using MinhaApi.Models;
+using MinhaApi.Repositories;
+using MinhaApi.Services;
+
+public class DepartamentoService : IDepartamentoService
+{
+  private readonly IDepartamentoRepository _repo;
+  //private readonly IFornecedorRepository _repoFornecedor;
+
+  public DepartamentoService(IDepartamentoRepository repo)
+    {
+        _repo = repo;
+        //_repoFornecedor = repoFornecedor;
+    } 
+
+  public IEnumerable<Departamento> GetAll()
+      => _repo.GetAll();
+
+  public Departamento? GetById(int id)
+      => _repo.GetById(id);
+
+  public Departamento Create(Departamento departamento)
+  {
+      if (departamento.Nome == null && departamento.Descricao == null)
+          throw new ArgumentException("Departamento inválido");
+
+      //if (departamento.Nome )
+      _repo.Add(departamento);
+      return departamento;
+  }
+
+  public Departamento? Update(int id, Departamento d)
+  {
+      if (_repo.GetById(id) == null) return null;
+      d.Id = id;
+      _repo.Update(d);
+      return d;
+  }
+
+  public bool Delete(int id)
+  {
+       // if (_repoFornecedor.GetById() )
+        if (_repo.GetById(id) == null) return false;
+            _repo.Delete(id);
+        return true;
+  }
+}
